9b87d0a60c006bb214b349ee698e92d739bef570
Felix Li <liyb.gz@gmail.com>
2017-11-12 13:22:49 +0800

    Add login page

--- project.py
+++ project.py
@@ -5,6 +5,10 @@ from sqlalchemy import create_engine, asc
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Restaurant, MenuItem
 
+# Add login session
+from flask import session as login_session
+import random, string
+
 
 #Connect to Database and create database session
 engine = create_engine('sqlite:///restaurantmenu.db')
@@ -137,6 +141,13 @@ def deleteMenuItem(restaurant_id,menu_id):
         return render_template('deleteMenuItem.html', item = itemToDelete)
 
 
+# Login
+@app.route('/login')
+def showLogin():
+  state = ''.join(random.choice(string.ascii_uppercase + string.digits)
+                  for x in xrange(32))
+  login_session['state'] = state
+  return render_template('login.html', STATE = state)
 
 
 if __name__ == '__main__':
